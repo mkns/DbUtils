c8d2a825239df8e77994f06cb8496341e6db72fc
Kenny Scott <kenny@kennyscott.co.uk>
2010-12-18 18:20:58 +0000

    db table and data dumped to sql/testdata.sql

---
+++ sql/testdata.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `Person` (
+  `name` varchar(255) default NULL,
+  `age` int(11) default NULL
+);
+
+INSERT INTO `Person` VALUES ('John Doe',25);
+INSERT INTO `Person` VALUES ('Bilbo Baggins',26);
+INSERT INTO `Person` VALUES ('John Doe',29);
